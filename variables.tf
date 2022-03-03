d4c60c1871f71ee5c70cc6614fb9a513b18b0a91
mohammedshashu <95119530+mohammedshashu@users.noreply.github.com>
2022-03-03 11:04:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "my-jenkins-security-group"
+  default     = "SRM-jenkins-security-group"
 }
 
 variable "tag_name" {
